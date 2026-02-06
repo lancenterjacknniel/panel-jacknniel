a4a2b1f351222fc8a7459d2ed332d21f58c84082
lancenterjacknniel <lancenterjacknnieladm@gmail.com>
2026-02-06 03:38:25 -0400

    Update app.js
    ascvz

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhR3XZX9EG1sLuOvBefZ2EA5GVn8-SggQ",
+  authDomain: "panel-jacknniel.firebaseapp.com",
+  projectId: "panel-jacknniel",
+  storageBucket: "panel-jacknniel.firebasestorage.app",
+  messagingSenderId: "130186632575",
+  appId: "1:130186632575:web:135d0352564c290d6d0e4a",
+  measurementId: "G-1QBZ9BNNLT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 function login(){
   let u=document.getElementById("user").value;
   let p=document.getElementById("pass").value;
