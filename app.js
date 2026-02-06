30ff37494b886fdfa2d75eb14ab5182c7c8fb5fb
lancenterjacknniel <lancenterjacknnieladm@gmail.com>
2026-02-06 12:54:57 -0400

    Update app.js
    1

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-function login() {
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.login = function () {
 
   let email = document.getElementById("user").value;
   let pass = document.getElementById("pass").value;
 
-  auth.signInWithEmailAndPassword(email, pass)
-    .then(() => {
+  signInWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+
+      const user = userCredential.user;
+
+      localStorage.setItem("user", user.email);
+
+      // ADMIN
+      if (user.email === "admin@lan.com") {
+        localStorage.setItem("rol", "admin");
+      } else {
+        localStorage.setItem("rol", "operador");
+      }
+
       window.location.href = "panel.html";
+
     })
     .catch(() => {
       document.getElementById("msg").innerText =
         "❌ Usuario o contraseña incorrecta";
     });
-}
+};
