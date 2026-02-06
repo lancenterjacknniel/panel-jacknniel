dc18eeb6d2b842ab3b9b26a5b61120cb9514f4ab
lancenterjacknniel <lancenterjacknnieladm@gmail.com>
2026-02-06 03:15:34 -0400

    Update usuarios.js
    USUARIOS

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,19 @@
-const usuarios = {
-  "Lancenterjacknniel": {pass:"0000000", rol:"admin"},
-  "Weymar": {pass:"1234", rol:"operador"},
-  "Javier": {pass:"5647", rol:"operador"}
-};
+// usuarios.js
+
+export const usuarios = [
+  {
+    user: "lancenterjacknniel",
+    pass: "0000000",
+    rol: "admin"
+  },
+  {
+    user: "Weymar",
+    pass: "1234",
+    rol: "operador"
+  },
+  {
+    user: "Javier",
+    pass: "5647",
+    rol: "operador"
+  }
+];
