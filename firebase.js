74916ec0f4c445aa96efcbeb91bfcd9512a1c061
lancenterjacknniel <lancenterjacknnieladm@gmail.com>
2026-02-06 13:33:08 -0400

    Create firebase.js
    1

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhR3XZX9EG1sLuOvBefZ2EA5GVn8-SggQ",
+  authDomain: "panel-jacknniel.firebaseapp.com",
+  projectId: "panel-jacknniel",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
